410d8490bdad684cf8ead5d9ead04941886db296
AbdelramanFahd <abdelrahmanfahd98@gmail.com>
2025-06-27 00:31:27 +0300

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: './tests/auth.env' });
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  workers: 1,
+  workers: 2,
   reporter: [
     ['html', { outputFolder: process.env.PLAYWRIGHT_HTML_REPORT || 'playwright-report' }]
   ],
