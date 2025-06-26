3555669a7ff5a0b7426385809e9d14ab8335212b
AbdelramanFahd <abdelrahmanfahd98@gmail.com>
2025-06-27 00:34:55 +0300

    ww

--- tests/example2.spec.ts
+++ tests/example2.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('light Mode', async ({ page }) => {
+  console.log('=============>'+process.env.PLAYWRIGHT_HTML_REPORT)
   await page.goto('https://www.google.com')
   if (process.env.market === 'IE')
     expect("Fahd").toBe('Ahmed')
